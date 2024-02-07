23d7712be014d8ca29dfb204824cfe8428c17683
Dewald <dewaldfourie0808@gmail.com>
2024-02-07 21:01:53 +0200

    added skeleton site for project with configs

---
+++ express-local-library/routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+// creating a new route, for testing purpose (http://localhost:3000/users/user1test)
+router.get('/user1test', function(req, res, next) {
+  res.send('This is the testing Route for user1');
+});
+
+module.exports = router;
