ee3bcc4f5fb8efb1fbb9b1da42f9045b92075392
Dewald <dewaldfourie0808@gmail.com>
2024-02-14 22:11:59 +0200

    moved templates from EJS to pug, added views

--- express-local-library/controllers/bookController.js
+++ express-local-library/controllers/bookController.js
@@ -1,13 +1,49 @@
 const Book = require("../models/book");
+const Author = require("../models/author");
+const Genre = require("../models/genre");
+const BookInstance = require("../models/bookinstance");
+
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Site Home Page");
+      // Get details of books, book instances, authors and genre counts (in parallel)
+    const [
+        numBooks,
+        numBookInstances,
+        numAvailableBookInstances,
+        numAuthors,
+        numGenres,
+    ] = await Promise.all([                  // Because the queries for document counts are independent, 
+        Book.countDocuments({}).exec(),                     // we use Promise.all, to run the in parallel                 
+        BookInstance.countDocuments({}).exec(),
+        BookInstance.countDocuments({ status: "Available" }).exec(),
+        Author.countDocuments({}).exec(),
+        Genre.countDocuments({}).exec(),
+    ]);
+
+    res.render("index", {
+        title: "Local Library Home",
+        book_count: numBooks,
+        book_instance_count: numBookInstances,
+        book_instance_available_count: numAvailableBookInstances,
+        author_count: numAuthors,
+        genre_count: numGenres,
+    });
 });
 
 // Display list of all books.
+/*The route handler calls the find() function on the Book model, 
+// selecting to return only the title and author as we don't need the other fields 
+//(it will also return the _id and virtual fields), and sorting the results by the title alphabetically using the sort() method. 
+// We also call populate() on Book, specifying the author field—this will replace the stored book author id with the full author details. 
+exec() is then daisy-chained on the end in order to execute the query and return a promise.*/
 exports.book_list = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Book list");
+    const allBooks = await Book.find({}, "title author")
+        .sort({ title: 1 })
+        .populate("author")
+        .exec();
+
+    res.render("book_list", { title: "Book List", book_list: allBooks })
 });
 
 // Display detail page for a specific book.
