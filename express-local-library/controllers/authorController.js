d40291ed2a2a286a578263bad30b2c39b90e1155
Dewald <dewaldfourie0808@gmail.com>
2024-02-17 09:07:35 +0200

    added book det, genre det, author det

--- express-local-library/controllers/authorController.js
+++ express-local-library/controllers/authorController.js
@@ -1,5 +1,6 @@
 
 const Author = require("../models/author");
+const Book = require("../models/book");
 const asyncHandler = require("express-async-handler");
 
 // Display list of ALL authors
@@ -14,7 +15,23 @@ exports.author_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific author
 exports.author_detail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+    const [author, allBookByAuthor] = await Promise.all([
+        Author.findById(req.params.id).exec(),
+        Book.find({ author: req.params.id }, "title summary").exec(),
+    ]);
+
+    if (author === null) {
+        // No results
+        const err =  new Error("Author not found");
+        err.status = 404;
+        return next(err)
+    }
+
+    res.render("author_detail", {
+        title: "Author Detail",
+        author: author,
+        author_books: allBookByAuthor,
+    });
 });
 
 // Display Author create form on GET
