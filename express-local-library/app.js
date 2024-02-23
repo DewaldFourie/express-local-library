217286b463a842f3707ddb0b95e39d3b46912d14
Dewald <dewaldfourie0808@gmail.com>
2024-02-23 08:30:33 +0200

    added production security measures and limitations

--- express-local-library/app.js
+++ express-local-library/app.js
@@ -7,13 +7,24 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+const compression = require("compression");
+const helmet = require("helmet");
 
 var app = express();
+
+// Set up rate limiter: max of 20 requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
 
 // set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://dewaldfourie0808:UotppdAIU2ASyltY@cluster0.ys3y9zh.mongodb.net/local_library?retryWrites=true&w=majority";
+const dev_db_url = "mongodb+srv://dewaldfourie0808:UotppdAIU2ASyltY@cluster0.ys3y9zh.mongodb.net/local_library?retryWrites=true&w=majority";
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -28,7 +39,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(limiter);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -38,6 +51,16 @@ app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 
 // error handler
 app.use(function(err, req, res, next) {
